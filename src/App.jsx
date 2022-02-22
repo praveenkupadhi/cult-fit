6eb618c463a3ba69a8ec0de1f5c8e0863b11f951
Jakaran <80308625+jaskaranproductions@users.noreply.github.com>
2022-02-22 09:31:20 +0530

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1>Hello</h1>
+      <section
+        style={{
+          height: "150vh",
+        }}
+      ></section>
+    </div>
+  );
+}
+
+export default App;
