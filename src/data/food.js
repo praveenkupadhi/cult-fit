72c7229bd3b37b035bb54cce942a1d2537d0ab80
Jakaran <80308625+jaskaranproductions@users.noreply.github.com>
2022-02-27 17:31:00 +0530

    data change

---
+++ src/data/food.js
@@ -0,0 +1,393 @@
+export const food = () => [
+  {
+    id: 1,
+    images: [
+      "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_600,ar_1.33,c_fit/dpr_2/image/singles/eat/meals/EAT6719/primary/1_1.jpg",
+    ],
+    tagName: "Meals",
+    title: "Whole Wheat Garlic Bread",
+    category: "Multigrain Pizzas",
+    price: 54,
+    cutPrice: 109,
+    Offers_applied: [
+      {
+        Discount: "Flat 50% discount on Meals T&C",
+        Fitcash: "Get 20% Fitcash on orders from ₹250 ( upto 80) T&C",
+      },
+    ],
+    Nutritional_Info: [
+      {
+        Protein: "9g PROTEIN",
+        Fat: "16g FAT",
+        Carbs: "31g CARBS",
+        Fibre: "2g FIBRE",
+      },
+    ],
+    description:
+      "An Italian meal isn't complete without a little cheesy garlic bread - that too this half moon delight is made with whole wheat flour! Enjoy anytime, anyday!",
+  },
+
+  {
+    id: 2,
+    images: [
+      "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_600,ar_1.33,c_fit/dpr_2/image/singles/eat/meals/EAT6612/primary/2_1.jpg",
+    ],
+    tagName: "Meals",
+    title: "Chicken Tikka Stuff Garlic Bread",
+    category: "Multigrain Pizzas",
+    price: 94,
+    cutPrice: 188,
+    Offers_applied: [
+      {
+        Discount: "Flat 50% discount on Meals T&C",
+        Fitcash: "Get 20% Fitcash on orders from ₹250 ( upto 80) T&C",
+      },
+    ],
+    Nutritional_Info: [
+      {
+        Protein: "15g PROTEIN",
+        Fat: "18g FAT",
+        Carbs: "63g CARBS",
+        Fibre: "4g FIBRE",
+      },
+    ],
+    description:
+      "A crowd favourite! High fibre garlic bread stuffed with chicken tikka, onions, capsicum, generous tandoori spread, yummy oregano seasoning and chilli flakes.",
+  },
+
+  {
+    id: 3,
+    images: [
+      "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_600,ar_1.33,c_fit/dpr_2/image/singles/eat/meals/EAT6614/primary/2_1.jpg",
+    ],
+    tagName: "Meals",
+    title: "Paneer Tikka Stuff Garlic Bread",
+    category: "Multigrain Pizzas",
+    price: 94,
+    cutPrice: 189,
+    Offers_applied: [
+      {
+        Discount: "Flat 50% discount on Meals T&C",
+        Fitcash: "Get 20% Fitcash on orders from ₹250 ( upto 80) T&C",
+      },
+    ],
+    Nutritional_Info: [
+      {
+        Protein: "15g PROTEIN",
+        Fat: "24g FAT",
+        Carbs: "66g CARBS",
+        Fibre: "5g FIBRE",
+      },
+    ],
+    description:
+      "A cheesy, scrumptious appetizer! High fibre garlic bread stuffed with paneer tikka, onions, capsicum, generous cheese sauce, yummy oregano seasoning and chilli flakes.",
+  },
+
+  {
+    id: 4,
+    images: [
+      "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_600,ar_1.33,c_fit/dpr_2/image/singles/eat/meals/EAT6672/primary/3_1.jpg",
+    ],
+    tagName: "Meals",
+    title: "8 Golden Corn Pizza",
+    category: "Multigrain Pizzas",
+    price: 136,
+    cutPrice: 272,
+    Offers_applied: [
+      {
+        Discount: "Flat 50% discount on Meals T&C",
+        Fitcash: "Get 20% Fitcash on orders from ₹250 ( upto 80) T&C",
+      },
+    ],
+    Nutritional_Info: [
+      {
+        Protein: "21g PROTEIN",
+        Fat: "16g FAT",
+        Carbs: "87g CARBS",
+        Fibre: "6g FIBRE",
+      },
+    ],
+    description:
+      "Multigrain pizza base, cheese sauce, mozarella - feta cheese, American corn.        What makes our pizza unique?  Our collection of artisanal pizzas have been crafted by our chefs, bringing together a soft base and handpicked premium toppings.Our mozzarella cheese is of the highest quality & taste that makes the pizza real good.  Ideal pizza pick for Corn lovers !!!!",
+  },
+
+  {
+    id: 5,
+    images: [
+      "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_600,ar_1.33,c_fit/dpr_2/image/singles/eat/meals/EAT6524/primary/4_1.jpg",
+    ],
+    tagName: "Meals",
+    title: "8 Roasted Veggie Pizza",
+    category: "Multigrain Pizzas",
+    price: 146,
+    cutPrice: 293,
+    Offers_applied: [
+      {
+        Discount: "Flat 50% discount on Meals T&C",
+        Fitcash: "Get 20% Fitcash on orders from ₹250 ( upto 80) T&C",
+      },
+    ],
+    Nutritional_Info: [
+      {
+        Protein: "21g PROTEIN",
+        Fat: "20g FAT",
+        Carbs: "80g CARBS",
+        Fibre: "9g FIBRE",
+      },
+    ],
+    description:
+      "Multigrain base, cheese sauce spread, topped with mushroom , capsicum, onion, tikka masala and mozarella cheese.  What makes our pizza unique?  Our collection of artisanal pizzas have been crafted by our chefs, bringing together a soft base and interesting premium toppings. Our mozzarella cheese is of the highest quality & taste that makes the pizza real good, A veg lover's delight !!!!",
+  },
+
+  {
+    id: 6,
+    images: [
+      "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_600,ar_1.33,c_fit/dpr_2/image/singles/eat/meals/EAT6394/primary/4_1.jpg",
+    ],
+    tagName: "Meals",
+    title: "8 Classic Paneer Pizza",
+    category: "Multigrain Pizzas",
+    price: 152,
+    cutPrice: 304,
+    Offers_applied: [
+      {
+        Discount: "Flat 50% discount on Meals T&C",
+        Fitcash: "Get 20% Fitcash on orders from ₹250 ( upto 80) T&C",
+      },
+    ],
+    Nutritional_Info: [
+      {
+        Protein: "27g PROTEIN",
+        Fat: "21g FAT",
+        Carbs: "89g CARBS",
+        Fibre: "7g FIBRE",
+      },
+    ],
+    description:
+      "Multigrain base, yumm paneer tikka, onion - capsicum topping and generous spread of cheese. What makes our pizza unique?  Our collection of artisanal pizzas have been crafted by our chefs, bringing together a soft base and delectable premium toppings. Our mozzarella cheese is of the highest quality & taste that makes the pizza real good. Ideal pizza pick for Paneer lovers !!!!",
+  },
+
+  {
+    id: 7,
+    images: [
+      "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_600,ar_1.33,c_fit/dpr_2/image/singles/eat/meals/EAT6545/primary/4_1.jpg",
+    ],
+    tagName: "Meals",
+    title: "Double Cheese Margherita Pizza",
+    category: "Multigrain Pizzas",
+    price: 157,
+    cutPrice: 314,
+    Offers_applied: [
+      {
+        Discount: "Flat 50% discount on Meals T&C",
+        Fitcash: "Get 20% Fitcash on orders from ₹250 ( upto 80) T&C",
+      },
+    ],
+    Nutritional_Info: [
+      {
+        Protein: "24g PROTEIN",
+        Fat: "21g FAT",
+        Carbs: "86g CARBS",
+        Fibre: "3g FIBRE",
+      },
+    ],
+    description:
+      "Multigrain base, double cheese topping - margherita and bocconcini. What makes our pizza unique?  Our collection of artisanal pizzas have been crafted by our chefs, bringing together a soft base and delectable premium toppings. Our mozzarella cheese is of the highest quality & taste that makes the pizza real good. Ideal pizza pick for Cheesy people !!!!",
+  },
+
+  {
+    id: 8,
+    images: [
+      "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_600,ar_1.33,c_fit/dpr_2/image/singles/eat/meals/EAT6395/primary/6_1.jpg",
+    ],
+    tagName: "Meals",
+    title: "UnMeat Veg Spicy Pizza",
+    category: "Multigrain Pizzas",
+    price: 157,
+    cutPrice: 314,
+    Offers_applied: [
+      {
+        Discount: "Flat 50% discount on Meals T&C",
+        Fitcash: "Get 20% Fitcash on orders from ₹250 ( upto 80) T&C",
+      },
+    ],
+    Nutritional_Info: [
+      {
+        Protein: "29g PROTEIN",
+        Fat: "22g FAT",
+        Carbs: "93g CARBS",
+        Fibre: "8g FIBRE",
+      },
+    ],
+    description:
+      "Multigrain base, tasty vegetable protein topping, tomato sauce spread and generous amount of cheese. What makes our pizza unique?  Our collection of artisanal pizzas have been crafted by our chefs, bringing together a soft base and delectable premium toppings. Our mozzarella cheese is of the highest quality & taste that makes the pizza real good. Ideal pizza pick for Veggie lovers",
+  },
+
+  {
+    id: 9,
+    images: [
+      "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_600,ar_1.33,c_fit/dpr_2/image/singles/eat/meals/COM518/primary/2_1.jpg",
+    ],
+    tagName: "Meals",
+    title: "UnMeat Veg Spicy Pizza & Zero Sugar Cola Fizz",
+    category: "Multigrain Pizzas",
+    price: 183,
+    cutPrice: 366,
+    Offers_applied: [
+      {
+        Discount: "Flat 50% discount on Meals T&C",
+        Fitcash: "Get 20% Fitcash on orders from ₹250 ( upto 80) T&C",
+      },
+    ],
+    Nutritional_Info: [
+      {
+        Protein: "30g PROTEIN",
+        Fat: "34g FAT",
+        Carbs: "88g CARBS",
+        Fibre: "9g FIBRE",
+      },
+    ],
+    description:
+      "Guilt free unmeat veg special pizza with multigrain base, tasty vegetable protein topping, tomato sauce spread and generous amount of cheese along with unbeatable taste and goodness of fizzy classic cola with superherb - Ashwagandha.",
+  },
+
+  {
+    id: 10,
+    images: [
+      "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_600,ar_1.33,c_fit/dpr_2/image/singles/eat/meals/EAT6221/primary/3_1.jpg",
+    ],
+    tagName: "Meals",
+    title: "Masala Egg curry, 2 Paratha Thali",
+    category: "Indaian Thalis",
+    price: 89,
+    cutPrice: 178,
+    Offers_applied: [
+      {
+        Discount: "Flat 50% discount on Meals T&C",
+        Fitcash: "Get 20% Fitcash on orders from ₹250 ( upto 80) T&C",
+      },
+    ],
+    Nutritional_Info: [
+      {
+        Protein: "26g PROTEIN",
+        Fat: "28g FAT",
+        Carbs: "59g CARBS",
+        Fibre: "15g FIBRE",
+      },
+    ],
+    description:
+      "Want a meal that's nutritious yet light on your stomach? This thali of egg curry, soft whole wheat parathas, cucumber carrot salad and crunchy sirka onions is not only packed with nutrients but also with deliciousness.Allergen information: Egg, gluten, vinegar",
+  },
+
+  {
+    id: 11,
+    images: [
+      "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_600,ar_1.33,c_fit/dpr_2/image/singles/eat/meals/EAT6443/primary/2_1.jpg",
+    ],
+    tagName: "Meals",
+    title: "Veg Keema, 2 Paratha Thali",
+    category: "Indian Thali",
+    price: 89,
+    cutPrice: 178,
+    Offers_applied: [
+      {
+        Discount: "Flat 50% discount on Meals T&C",
+        Fitcash: "Get 20% Fitcash on orders from ₹250 ( upto 80) T&C",
+      },
+    ],
+    Nutritional_Info: [
+      {
+        Protein: "26g PROTEIN",
+        Fat: "28g FAT",
+        Carbs: "59g CARBS",
+        Fibre: "15g FIBRE",
+      },
+    ],
+    description:
+      "A quick and delicious light thali! Enjoy soft parathas with mildly spiced veg keema - a combination of tofu, soya granules, capsicum cooked with home made spices and topped with roasted peanuts. Served with carrot cucumber salad and sirka onion",
+  },
+
+  {
+    id: 12,
+    images: [
+      "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_600,ar_1.33,c_fit/dpr_2/image/singles/eat/meals/EAT6631/primary/2_1.jpg",
+    ],
+    tagName: "Meals",
+    title: "Ghar ke Rajma, Aloo Methi, Roti Thali",
+    category: "Indian Thali",
+    price: 104,
+    cutPrice: 204,
+    Offers_applied: [
+      {
+        Discount: "Flat 50% discount on Meals T&C",
+        Fitcash: "Get 20% Fitcash on orders from ₹250 ( upto 80) T&C",
+      },
+    ],
+    Nutritional_Info: [
+      {
+        Protein: "26g PROTEIN",
+        Fat: "28g FAT",
+        Carbs: "89g CARBS",
+        Fibre: "22g FIBRE",
+      },
+    ],
+    description:
+      "Mildly spiced, juicy mix of ghar ke rajma has been paired with delicious aloo methi and soft whole wheat rotis. Sit back, relax and savour this comforting meal.",
+  },
+
+  {
+    id: 13,
+    images: [
+      "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_600,ar_1.33,c_fit/dpr_2/image/singles/eat/meals/EAT6653/primary/2_1.jpg",
+    ],
+    tagName: "Meals",
+    title: "Achari Khichdi (500gm)",
+    category: "khichdi",
+    price: 89,
+    cutPrice: 178,
+    Offers_applied: [
+      {
+        Discount: "Flat 50% discount on Meals T&C",
+        Fitcash: "Get 20% Fitcash on orders from ₹250 ( upto 80) T&C",
+      },
+    ],
+    Nutritional_Info: [
+      {
+        Protein: "10g PROTEIN",
+        Fat: "17g FAT",
+        Carbs: "59g CARBS",
+        Fibre: "5g FIBRE",
+      },
+    ],
+    description:
+      "A flavourful South Indian khichdi! This dal protein packed khichdi has an achaari touch. Topped with ghee.",
+  },
+
+  {
+    id: 14,
+    images: [
+      "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_600,ar_1.33,c_fit/dpr_2/image/singles/eat/meals/EAT6651/primary/1_1.jpg",
+    ],
+    tagName: "Meals",
+    title: "Bisi Bele Bath (500gm)",
+    category: "khichdis",
+    price: 89,
+    cutPrice: 178,
+    Offers_applied: [
+      {
+        Discount: "Flat 50% discount on Meals T&C",
+        Fitcash: "Get 20% Fitcash on orders from ₹250 ( upto 80) T&C",
+      },
+    ],
+    Nutritional_Info: [
+      {
+        Protein: "13g PROTEIN",
+        Fat: "26g FAT",
+        Carbs: "69g CARBS",
+        Fibre: "9g FIBRE",
+      },
+    ],
+    description:
+      "A south Indian favourite, made with wholesome, high protein dal, rice and vegetables like  pumpkin, brinjal, carrots and radish, topped with ghee tadka.",
+  },
+];
---
+++ src/data/fresh_fruits.js
@@ -0,0 +1,197 @@
+export const data = () => [
+  {
+    id: 1,
+    images: [
+      "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_600,ar_1.33,c_fit/dpr_2/image/singles/eat/meals/EAT6360/primary/4_1.jpg",
+    ],
+    tagName: "Meals",
+    title: "Fruit Bowl",
+    category: "Fresh Fruits",
+    price: 99,
+    cutPrice: 199,
+    Offers_applied: [
+      {
+        Discount: "Flat 50% discount on Meals T&C",
+        Fitcash: "Get 20% Fitcash on orders from ₹250 ( upto 80) T&C",
+      },
+    ],
+    Nutritional_Info: [
+      {
+        Protein: "4g PROTEIN",
+        Fat: "2g FAT",
+        Carbs: "53g CARBS",
+        Fibre: "6g FIBRE",
+      },
+    ],
+    description:
+      "Get your dose of natural sugars, fibre and immunity-boosting antioxidants with our fruit bowl packed with our hydrating seasonal fruits. The special addition of nuts and seeds add crunch and flavor to the fruit bowl. ",
+  },
+
+  {
+    id: 2,
+    images: [
+      "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_600,ar_1.33,c_fit/dpr_2/image/singles/eat/meals/EAT6360/primary/4_1.jpg",
+    ],
+    tagName: "Meals",
+    title: "Fruit Bowl",
+    category: "Fresh Fruits",
+    price: 99,
+    cutPrice: 199,
+    Offers_applied: [
+      {
+        Discount: "Flat 50% discount on Meals T&C",
+        Fitcash: "Get 20% Fitcash on orders from ₹250 ( upto 80) T&C",
+      },
+    ],
+    Nutritional_Info: [
+      {
+        Protein: "4g PROTEIN",
+        Fat: "2g FAT",
+        Carbs: "53g CARBS",
+        Fibre: "6g FIBRE",
+      },
+    ],
+    description:
+      "Get your dose of natural sugars, fibre and immunity-boosting antioxidants with our fruit bowl packed with our hydrating seasonal fruits. The special addition of nuts and seeds add crunch and flavor to the fruit bowl. ",
+  },
+
+  {
+    id: 3,
+    images: [
+      "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_600,ar_1.33,c_fit/dpr_2/image/singles/eat/meals/EAT6360/primary/4_1.jpg",
+    ],
+    tagName: "Meals",
+    title: "Fruit Bowl",
+    category: "Fresh Fruits",
+    price: 99,
+    cutPrice: 199,
+    Offers_applied: [
+      {
+        Discount: "Flat 50% discount on Meals T&C",
+        Fitcash: "Get 20% Fitcash on orders from ₹250 ( upto 80) T&C",
+      },
+    ],
+    Nutritional_Info: [
+      {
+        Protein: "4g PROTEIN",
+        Fat: "2g FAT",
+        Carbs: "53g CARBS",
+        Fibre: "6g FIBRE",
+      },
+    ],
+    description:
+      "Get your dose of natural sugars, fibre and immunity-boosting antioxidants with our fruit bowl packed with our hydrating seasonal fruits. The special addition of nuts and seeds add crunch and flavor to the fruit bowl. ",
+  },
+
+  {
+    id: 4,
+    images: [
+      "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_600,ar_1.33,c_fit/dpr_2/image/singles/eat/meals/EAT6360/primary/4_1.jpg",
+    ],
+    tagName: "Meals",
+    title: "Fruit Bowl",
+    category: "Fresh Fruits",
+    price: 99,
+    cutPrice: 199,
+    Offers_applied: [
+      {
+        Discount: "Flat 50% discount on Meals T&C",
+        Fitcash: "Get 20% Fitcash on orders from ₹250 ( upto 80) T&C",
+      },
+    ],
+    Nutritional_Info: [
+      {
+        Protein: "4g PROTEIN",
+        Fat: "2g FAT",
+        Carbs: "53g CARBS",
+        Fibre: "6g FIBRE",
+      },
+    ],
+    description:
+      "Get your dose of natural sugars, fibre and immunity-boosting antioxidants with our fruit bowl packed with our hydrating seasonal fruits. The special addition of nuts and seeds add crunch and flavor to the fruit bowl. ",
+  },
+
+  {
+    id: 5,
+    images: [
+      "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_600,ar_1.33,c_fit/dpr_2/image/singles/eat/meals/EAT6360/primary/4_1.jpg",
+    ],
+    tagName: "Meals",
+    title: "Fruit Bowl",
+    category: "Fresh Fruits",
+    price: 99,
+    cutPrice: 199,
+    Offers_applied: [
+      {
+        Discount: "Flat 50% discount on Meals T&C",
+        Fitcash: "Get 20% Fitcash on orders from ₹250 ( upto 80) T&C",
+      },
+    ],
+    Nutritional_Info: [
+      {
+        Protein: "4g PROTEIN",
+        Fat: "2g FAT",
+        Carbs: "53g CARBS",
+        Fibre: "6g FIBRE",
+      },
+    ],
+    description:
+      "Get your dose of natural sugars, fibre and immunity-boosting antioxidants with our fruit bowl packed with our hydrating seasonal fruits. The special addition of nuts and seeds add crunch and flavor to the fruit bowl. ",
+  },
+
+  {
+    id: 6,
+    images: [
+      "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_600,ar_1.33,c_fit/dpr_2/image/singles/eat/meals/EAT6360/primary/4_1.jpg",
+    ],
+    tagName: "Meals",
+    title: "Fruit Bowl",
+    category: "Fresh Fruits",
+    price: 99,
+    cutPrice: 199,
+    Offers_applied: [
+      {
+        Discount: "Flat 50% discount on Meals T&C",
+        Fitcash: "Get 20% Fitcash on orders from ₹250 ( upto 80) T&C",
+      },
+    ],
+    Nutritional_Info: [
+      {
+        Protein: "4g PROTEIN",
+        Fat: "2g FAT",
+        Carbs: "53g CARBS",
+        Fibre: "6g FIBRE",
+      },
+    ],
+    description:
+      "Get your dose of natural sugars, fibre and immunity-boosting antioxidants with our fruit bowl packed with our hydrating seasonal fruits. The special addition of nuts and seeds add crunch and flavor to the fruit bowl. ",
+  },
+
+  {
+    id: 7,
+    images: [
+      "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_600,ar_1.33,c_fit/dpr_2/image/singles/eat/meals/EAT6360/primary/4_1.jpg",
+    ],
+    tagName: "Meals",
+    title: "Fruit Bowl",
+    category: "Fresh Fruits",
+    price: 99,
+    cutPrice: 199,
+    Offers_applied: [
+      {
+        Discount: "Flat 50% discount on Meals T&C",
+        Fitcash: "Get 20% Fitcash on orders from ₹250 ( upto 80) T&C",
+      },
+    ],
+    Nutritional_Info: [
+      {
+        Protein: "4g PROTEIN",
+        Fat: "2g FAT",
+        Carbs: "53g CARBS",
+        Fibre: "6g FIBRE",
+      },
+    ],
+    description:
+      "Get your dose of natural sugars, fibre and immunity-boosting antioxidants with our fruit bowl packed with our hydrating seasonal fruits. The special addition of nuts and seeds add crunch and flavor to the fruit bowl. ",
+  },
+];
